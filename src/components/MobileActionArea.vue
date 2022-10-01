9042f15553d3a5cb9fb66ef660254fa6f8f08c31
hsuifang <whalekiller03@gmail.com>
2022-10-01 12:43:13 +0800

    fix: components - mobileActionArea in laptop view

--- src/components/MobileActionArea.vue
+++ src/components/MobileActionArea.vue
@@ -6,7 +6,7 @@ const $store = useCartStore()
 </script>
 
 <template>
-  <div class="container bg-primary position-fixed bottom-0 py-2" style="z-index: 2">
+  <div class="container bg-primary position-fixed bottom-0 py-2 d-md-none" style="z-index: 2">
     <div class="row">
       <div class="col">
         <button
